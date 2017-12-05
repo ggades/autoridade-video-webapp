6cb050619fa2f9a66bfcbb93edba802cd36d4391
Guilherme Gades <guilherme.gades@gmail.com>
2017-12-05 01:34:35 -0200

    Removed react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
 import Home from '../containers/HomeContainer';
-import NotFound from './NotFound.jsx';
 import './../style/main.less';
 
 const App = ({ store }) => (
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={Home} />
-      <Route path="*" component={NotFound} />
-    </Router>
+    <Home />
   </Provider>
 );
 
